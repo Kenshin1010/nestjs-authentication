98e5a9eef2f4dc5233793641bb096513e5591680
anh.do <tuananh9h@gmail.com>
2024-07-30 00:09:36 +0700

    Login, Signup, Refresh Tokens, JWT, Guards

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignupDto } from './dtos/signup.dto';
+import { LoginDto } from './dtos/login.dto';
+import { RefreshTokenDto } from './dtos/refresh-token.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  // TODO: POST Signup
+  @Post('signup')
+  async signUp(@Body() signupData: SignupDto) {
+    return this.authService.signup(signupData);
+  }
+
+  // TODO: POST Login
+  @Post('login')
+  async login(@Body() credentials: LoginDto) {
+    return this.authService.login(credentials);
+  }
+
+  // TODO: POST Refresh Token
+  @Post('refresh')
+  async refreshTokens(@Body() refreshTokenDto: RefreshTokenDto) {
+    return this.authService.refreshTokens(refreshTokenDto.refreshToken);
+  }
 }
